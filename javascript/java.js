b71850c3cc95aec0ef420afc73b47b61c9d2ac22
SaverLINK48 <jgarci52@hawk.iit.edu>
2018-11-29 12:48:11 -0600

    Added the javascript file for the form and linked it.

---
+++ javascript/java.js
@@ -0,0 +1,5 @@
+function myFunction() {
+  alert("Thank you for submitting. You have been added to our e-mail list.") 
+  window.location.replace("https://jose-garcia-itmd-362.github.io/itmd362-project1/");
+    return false;
+}
